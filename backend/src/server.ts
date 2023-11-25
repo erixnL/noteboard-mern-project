b02b9faeba761f8f98f5acd226cafb2c01bfba8e
erixnL <96608889+erixnL@users.noreply.github.com>
2023-11-25 23:21:05 +1100

    connect to mongoDB; seperate evn config

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,25 @@
+import "dotenv/config";
+import env from "./until/validateEnv"
+import mongoose from "mongoose";
 import express from "express";
 const app = express();
-const port = 8000;
 
 app.get("/", (req, res)=> {
     res.send("Hello New World");
 });
 
-//start the server
-app.listen(port, ()=> {
-    console.log("server running on port: " + port);
+const port = env.PORT;
+
+mongoose.connect(env.MONGO_CONNECTION_STRING!)
+.then(()=> {
+    console.log("Mongoose connected");
+    //start the server
+    app.listen(port, ()=> {
+        console.log("server running on port: " + port);
+    });
 })
+.catch(console.error)
+;
+
+
+
