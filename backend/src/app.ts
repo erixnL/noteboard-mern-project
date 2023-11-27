97cfc0cb3da29b6ee06a617d26424113f1d4101f
erixnL <96608889+erixnL@users.noreply.github.com>
2023-11-27 21:24:06 +1100

    define Note type;add env config;error handler middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,27 @@
 import "dotenv/config";
-import express from "express";
+import express, {NextFunction, Request, Response} from "express";
+import NoteModel from "./models/note";
 
 const app = express();
 
-app.get("/", (req, res)=> {
-    res.send("Hello New World");
+app.get("/", async (req, res, next)=> {
+    try {
+        const notes = await NoteModel.find().exec();
+        res.status(200).json(notes);
+    } catch (error) {
+        //middleware create end points
+        next(error);
+    }
 });
+
+//setup error handler, middleware function
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction)=> {
+    console.error(error);
+    let errorMessage = "An unknown erro occurred";
+    if (error instanceof Error) errorMessage = error.message;
+    //return the error
+    res.status(500).json({error: errorMessage})
+})
 
 export default app;
---
+++ backend/src/models/note.ts
@@ -0,0 +1,13 @@
+import { InferSchemaType, model, Schema } from "mongoose";
+
+const noteSchema = new Schema({
+    title: { type: String, required: true },
+    text: { type: String },
+    //timestamp for createdAt and updatedAt
+}, {timestamps: true});
+
+//create type inferred from Mongoose schema
+type Note = InferSchemaType<typeof noteSchema>
+
+//model("nameOfCollection", collectionSchema)
+export default model<Note>("Note", noteSchema)
