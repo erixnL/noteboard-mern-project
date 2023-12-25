4af3e87765f094c39be44608d6eab1ad35ac6f86
erixnL <96608889+erixnL@users.noreply.github.com>
2023-12-25 22:36:04 +1100

    use express session middleware to remember user login;new user signup and user schema in database;user signup/login/login endpoints routing

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,12 +4,30 @@ import notesRoutes from "./routes/notes";
 import morgan from "morgan";
 import createHttpError, { isHttpError } from "http-errors";
 import userRoutes from "./routes/users";
+import session from "express-session";
+import env from "./until/validateEnv";
+import MongoStore from "connect-mongo";
+
 
 const app = express();
 
 app.use(morgan("dev"));
 //information type accepted to the server
 app.use(express.json());
+
+//user session config
+app.use(session({
+    secret: env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 60 * 60 * 1000,
+    },
+    rolling: true,
+    store: MongoStore.create({
+        mongoUrl: env.MONGO_CONNECTION_STRING
+    }),
+}));
 
 app.use("/api/users", userRoutes);
 
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -3,6 +3,12 @@ import * as userController from "../controllers/users";
 
 const router = express.Router();
 
+router.get("/", userController.getAuthenticatedUser);
+
 router.post("/signup", userController.signUp);
+
+router.post("/login", userController.longin);
+
+router.post("/logout", userController.logout);
 
 export default router;
